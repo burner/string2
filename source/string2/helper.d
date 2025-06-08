3a5cbb366ced46f1481f11b5740271289545e3cd
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2025-06-08 13:33:49 +0200

    remove unused function

--- source/string2/helper.d
+++ source/string2/helper.d
@@ -1,19 +1,10 @@
 module string2.helper;
 
-auto assumePure(T)(T t) @safe pure {
-    import std.traits : FunctionAttribute, SetFunctionAttributes
-        , isFunctionPointer, isDelegate, functionAttributes, functionLinkage;
+import std.traits : FunctionAttribute, SetFunctionAttributes
+    , isFunctionPointer, isDelegate, functionAttributes, functionLinkage;
 
+auto assumePure(T)(T t) @safe pure {
     static assert(isFunctionPointer!T || isDelegate!T);
     enum attrs = functionAttributes!T | FunctionAttribute.pure_;
     return () @trusted { return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs))t; }();
-}
-
-auto assumePureFun(T)(T t) @safe pure {
-    import std.traits : FunctionAttribute, SetFunctionAttributes
-        , isFunctionPointer, isDelegate, functionAttributes, functionLinkage;
-
-    static assert(isFunctionPointer!T || isDelegate!T);
-    enum attrs = functionAttributes!T | FunctionAttribute.pure_;
-    return () @trusted { return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs))t; };
 }
